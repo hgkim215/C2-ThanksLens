f348220942a8d77bbff09aa0967a6a159e9a9044
김현기 <hyeongikim@hyeongikimui-MacBookAir.local>
2025-04-18 23:48:20 +0900

    [Feat] 감사폴라로이드 컴포넌트 구현

--- ThanksLens/Feature/Home/Gallery/GalleryView.swift
+++ ThanksLens/Feature/Home/Gallery/GalleryView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+// MARK: - 갤러리뷰 (메인홈탭)
+
+/// 갤러리 화면을 나타내는 뷰로, 상단 날짜 선택 뷰와 폴라로이드 리스트를 포함합니다.
 struct GalleryView: View {
   @EnvironmentObject private var galleryViewModel: GalleryViewModel
 
@@ -18,60 +21,12 @@ struct GalleryView: View {
         .ignoresSafeArea(.all)
 
       VStack {
-        HStack {
-          Button(
-            action: {
-              galleryViewModel.selectedDate = galleryViewModel.selectedDate.addingTimeInterval(-86400)
-            },
-            label: {
-              Image(systemName: "chevron.left")
-                .foregroundStyle(.customP1)
-            }
-          )
-
-          Spacer()
-
-          Text(galleryViewModel.formattedDateString(from: galleryViewModel.selectedDate))
-            .font(.PoorStory(size: 24))
-            .foregroundStyle(.customP1)
-
-          Spacer()
-
-          Button(
-            action: {
-              galleryViewModel.selectedDate = galleryViewModel.selectedDate
-                .addingTimeInterval(86400)
-            },
-            label: {
-              Image(systemName: "chevron.right")
-                .foregroundStyle(.customP1)
-            }
-          )
-        }
-        .padding(.vertical, 24)
-        .padding(.horizontal, 32)
-
-        Spacer()
+        TopDateSelectionView()
 
         ScrollView {
           LazyVGrid(columns: columns, spacing: 24) {
             ForEach(galleryViewModel.thanksPolaroids, id: \.self) { polaroid in
-              ZStack {
-                Color(.customWhite)
-                VStack {
-                  Image("mock_image")
-                    .resizable()
-                    .frame(height: 200) // Set the height
-                    .aspectRatio(3 / 4, contentMode: .fill)
-                    .padding(.horizontal, 12)
-                    .padding(.top, 12)
-
-                  Text(polaroid.titleText)
-                    .foregroundColor(.customBlack)
-                    .font(.Gaegu_Bold(size: 14))
-                    .padding(12)
-                }
-              }
+              PolaroidCell(polaroid: polaroid)
             }
           }
           .padding(.horizontal, 16)
@@ -84,10 +39,73 @@ struct GalleryView: View {
   }
 }
 
-extension GalleryView {
-  private func emoji(_ value: Int) -> String {
-    guard let scalar = UnicodeScalar(value) else { return "?" }
-    return String(Character(scalar))
+// MARK: - 상단 날짜선택뷰
+
+/// 상단에 날짜를 선택할 수 있는 뷰로, 이전/다음 날짜로 이동하는 버튼과 현재 날짜를 표시합니다.
+private struct TopDateSelectionView: View {
+  @EnvironmentObject private var galleryViewModel: GalleryViewModel
+
+  fileprivate var body: some View {
+    HStack {
+      Button(
+        action: {
+          galleryViewModel.selectedDate = galleryViewModel.selectedDate.addingTimeInterval(-86400)
+        },
+        label: {
+          Image(systemName: "chevron.left")
+            .foregroundStyle(.customP1)
+        }
+      )
+
+      Spacer()
+
+      Text(galleryViewModel.formattedDateString(from: galleryViewModel.selectedDate))
+        .font(.PoorStory(size: 24))
+        .foregroundStyle(.customP1)
+
+      Spacer()
+
+      Button(
+        action: {
+          galleryViewModel.selectedDate = galleryViewModel.selectedDate
+            .addingTimeInterval(86400)
+        },
+        label: {
+          Image(systemName: "chevron.right")
+            .foregroundStyle(.customP1)
+        }
+      )
+    }
+    .padding(.vertical, 24)
+    .padding(.horizontal, 32)
+
+    Spacer()
+  }
+}
+
+// MARK: - 감사폴라로이드셀뷰
+
+/// 개별 폴라로이드 셀을 나타내는 뷰로, 이미지와 제목 텍스트를 포함합니다.
+private struct PolaroidCell: View {
+  let polaroid: ThanksPolaroid
+
+  fileprivate var body: some View {
+    ZStack {
+      Color(.customWhite)
+      VStack {
+        Image("mock_image")
+          .resizable()
+          .frame(width: 150, height: 200)
+          .aspectRatio(3 / 4, contentMode: .fit)
+          .padding(.horizontal, 12)
+          .padding(.top, 12)
+
+        Text(polaroid.titleText)
+          .foregroundColor(.customBlack)
+          .font(.Gaegu_Bold(size: 14))
+          .padding(12)
+      }
+    }
   }
 }
 
