509647c55396f07d4c34480baf34ad6b59a709f0
김현기 <hyeongikim@hyeongikimui-MacBookAir.local>
2025-04-21 16:19:24 +0900

    [Feat] 캘린더 기본 UI 구현 및 캘린더 사진 업로드 기본 기능 구현

--- ThanksLens/Feature/Home/Calendar/CalendarView.swift
+++ ThanksLens/Feature/Home/Calendar/CalendarView.swift
@@ -8,12 +8,61 @@
 import SwiftUI
 
 struct CalendarView: View {
+  @State private var selectedDate = Date()
+  @State private var selectedImage: UIImage?
+
   var body: some View {
     ZStack {
       Color(Color.customP4)
         .ignoresSafeArea(.all)
+      VStack {
+        GeometryReader { geometry in
+          let width = geometry.size.width
+          let imageHeight = width
 
-      Text("CalendarView 입니다.")
+          ThanksPhotoPicker(selctedImage: $selectedImage) {
+            ZStack {
+              if let image = selectedImage {
+                Image(uiImage: image)
+                  .resizable()
+                  .scaledToFill()
+                  .frame(width: width, height: imageHeight)
+                  .clipped()
+              }
+              if selectedImage == nil {
+                Image("calendar_image")
+                  .frame(width: width, height: imageHeight)
+              }
+            }
+            .overlay(
+              Rectangle()
+                .stroke(Color.customBlack, lineWidth: 2)
+                .blur(radius: 1)
+                .offset(x: 0, y: 4)
+                .mask(
+                  Rectangle()
+                    .fill(LinearGradient(
+                      colors: [.black.opacity(0.3), .clear],
+                      startPoint: .top,
+                      endPoint: .bottom
+                    ))
+                )
+            )
+          }
+        }
+        .frame(height: UIScreen.main.bounds.width - 16)
+        .ignoresSafeArea(edges: .top)
+
+        DatePicker(
+          "날짜를 선택하세요",
+          selection: $selectedDate,
+          displayedComponents: [.date]
+        )
+        .datePickerStyle(.graphical)
+        .tint(.customP1)
+      }
+      .padding(.horizontal, 16)
+      .padding(.vertical, 32)
     }
   }
 }
